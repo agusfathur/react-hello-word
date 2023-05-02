3c80b316590cd91e2375014d6792ad71a8bde581
Moh Agus Fathur Rozi <gusfathur25@gmail.com>
2023-05-02 21:52:31 +0700

    #4 Membuat Project React dengan create react app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const HelloWorld = () => {
+  return <p>Hello World</p>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HelloWorld />
+    {/* <br /> */}
+    <p>Hello World from ReactDOM</p>
   </React.StrictMode>
 );
 
